18a553a899ad969c2b0dbfce1f00014f3cce0663
Anastasia Labadze <66730967+ITulku@users.noreply.github.com>
2020-06-30 23:11:23 +0300

    Revert "-"
    This reverts commit 1a82fd66cec0aa2a8a3eb403027e4f875c69c6bc.

--- menu.php
+++
@@ -1,8 +0,0 @@
-
-	<div class="header">
-		<a href="#">Главная</a>
-		<a href="riddles.html">Загадки</a>
-		<a href="guess_number.html">Угадай число</a>
-		<a href="guess_number_2.html">Угадай число: сыграйте вдвоём</a>
-		<a href="pwgen.html">Генератор паролей</a>
-	</div>
